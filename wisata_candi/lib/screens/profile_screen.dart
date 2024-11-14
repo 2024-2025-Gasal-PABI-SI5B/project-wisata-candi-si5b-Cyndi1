0bedcd5056cebcd82e8383b6a764110a62ed85c8
Cyndi1 <cyndiclaudiawang_2226240125@mhs.mdp.ac.id>
2024-11-14 08:59:48 +0700

    bersambung, OTW part 4

--- wisata_candi/lib/screens/profile_screen.dart
+++ wisata_candi/lib/screens/profile_screen.dart
@@ -9,10 +9,24 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   // TODO : 1. Deskripsi variabel yang dibutuhkan
-  bool isSignedIn = true;
-  String fullName = '';
-  String userName = '';
+  bool isSignedIn = false;
+  String fullName = 'Cyndi Claudia';
+  String userName = 'cyndicw';
   int favoriteCandiCount = 0;
+
+  //TODO : 5. Implementasi fungsi SignIn
+  void signIn(){
+    setState(() {
+      isSignedIn = !isSignedIn;
+    });
+  }
+
+  //TODO : 6. Implementasi fungsi SignOut
+  void signOut(){
+    setState(() {
+      isSignedIn = !isSignedIn;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +42,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: [
-                // TODO : 2. Buat Bagian Profile header
+                // TODO : 2. Buat Bagian Profile header (isinya gambar profil)
                 Align(
                   alignment: Alignment.topCenter,
                   child: Padding(
@@ -43,7 +57,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                                 width: 2,
                               ),
                               shape: BoxShape.circle),
-                          child: CircleAvatar(
+                          child: const CircleAvatar(
                             radius: 50,
                             backgroundImage:
                                 AssetImage('images/placeholder_image.png'),
@@ -59,9 +73,86 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       ],
                     ),
                   ),
-                )
+                ),
                 // TODO : 3. Buat Bagian Profile Info (isinya info profil)
+                // Baris satu Pengguna
+                const SizedBox(height: 20,),
+                Divider(color: Colors.deepPurple[100],),
+                const SizedBox(height: 4,),
+                Row(
+                  children:[
+                     SizedBox(
+                      width: MediaQuery.of(context).size.width/3,
+                      child:const Row(
+                        children:[
+                          Icon(Icons.lock, color: Colors.amber,),
+                          const SizedBox(width: 8,),
+                          Text('Pengguna', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(': $userName',
+                        style : const TextStyle(fontSize: 18),
+                      )),
+                  ],
+                ),
+                // Baris dua Nama
+                const SizedBox(height: 4,),
+                Divider(color: Colors.deepPurple[100],),
+                const SizedBox(height: 4,),
+                Row(
+                  children:[
+                     SizedBox(
+                      width: MediaQuery.of(context).size.width/3,
+                      child:const Row(
+                        children:[
+                          Icon(Icons.person, color: Colors.blue,),
+                          const SizedBox(width: 8,),
+                          Text('Pengguna', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(': $fullName',
+                        style : const TextStyle(fontSize: 18),
+                      )),
+                      if (isSignedIn)
+                        const Icon(Icons.edit),
+                  ],
+                ),
+                // Baris tiga favorit
+                const SizedBox(height: 4,),
+                Divider(color: Colors.deepPurple[100],),
+                const SizedBox(height: 4,),
+                Row(
+                  children:[
+                     SizedBox(
+                      width: MediaQuery.of(context).size.width/3,
+                      child:const Row(
+                        children:[
+                          Icon(Icons.favorite, color: Colors.red,),
+                          const SizedBox(width: 8,),
+                          Text('Pengguna', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(': $favoriteCandiCount',
+                        style : const TextStyle(fontSize: 18),
+                      )
+                    ),
+                  ],
+                ),
                 // TODO : 4. Buat Bagian Profile Action (isinya tombil sign in / sign out)
+                const SizedBox(height: 4,),
+                Divider(color: Colors.deepPurple[100],),
+                const SizedBox(height: 20,),
+                
+                isSignedIn 
+                  ? TextButton(
+                    onPressed: signOut, child: Text('Sign Out'))
+                  : ElevatedButton(onPressed: signIn, child: Text('Sign In')),
               ],
             ),
           ),
